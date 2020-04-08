567b71fe6760b341151f57fed07560fe99cfd6f0
Shawn Nunoo <shawnnunoo@gmail.com>
2020-04-07 23:24:51 -0700

    added schema and routes to server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const server = app.listen(9000);
+const server = app.listen(4200);
 var path = require("path");
 var bodyParser = require('body-parser');
 const flash = require('express-flash')
 
 var mongoose = require('mongoose');
-
-
 
 mongoose.connect('mongodb://localhost/orders', {
     useNewUrlParser: true,
@@ -15,7 +13,7 @@ mongoose.connect('mongodb://localhost/orders', {
 });
 mongoose.Promise = global.Promise;
 
-app.use(express.static( __dirname + '/public/dist/public' ));
+app.use(express.static(__dirname + '/public/dist/public'));
 app.use(express.static(__dirname + "/static"));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -28,30 +26,93 @@ app.use(bodyParser.urlencoded({
 
 
 
-
-
-
-
 // ========================== 
 // Schemas
 // ========================== 
 
-
-
+var OrderSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    qty: {
+        type: Number
+    },
+}, {
+    timestamps: true
+})
+mongoose.model('Order', OrderSchema);
+var Order = mongoose.model('Order');
 
 
 
 // ========================== 
-// Routes
+// Routes to show all oders
 // ========================== 
 
-app.get('/', (req, res) => {
-   
+app.get('/vieworders', (req, res) => {
+    Order.find({}, (err, orders) => {
+        if (err) {
+            console.log("returned error", err);
+            res.json({
+                message: "error",
+                error: err
+            })
+        } else {
+            res.json({
+                message: "success",
+                data: orders
+            })
+        }
+    })
+})
+
+// ========================== 
+// Route to add to oder
+// ========================== 
+
+app.post('/add', (req, res) => {
+    Order.create(req.body, (err, orders) => {
+        if (err) {
+            console.log("returned error", err);
+            res.json({
+                message: "error",
+                error: err
+            })
+        } else {
+            res.json({
+                message: "success",
+                data: orders
+            })
+        }
+    })
+})
+
+// ========================== 
+// Route to delete to oder
+// ========================== 
+
+app.delete('vieworders/:id', (req, res) => {
+    Order.findByIdAndRemove(req.params.id, (err) => {
+        if (err) {
+            console.log("Returned error", err);
+            // respond with JSON
+            res.json({
+                message: "Error",
+                error: err
+            })
+        } else {
+            // respond with JSON
+            res.json({
+                message: "Success"
+            })
+        }
+    })
 })
 
 
-
-
+// ========================== 
+// 404 re-routing
+// ========================== 
 app.all("*", (req, res, next) => {
     res.sendFile(path.resolve("./public/dist/public/index.html"))
-  });
+});
