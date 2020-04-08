b56201420552ccaaddf2948363f6255f25abcb7b
Shawn Nunoo <shawnnunoo@gmail.com>
2020-04-07 22:56:09 -0700

    fixed server.js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
 const server = app.listen(9000);
-const io = require('socket.io')(server);
 var path = require("path");
 var bodyParser = require('body-parser');
-var session = require('express-session');
 const flash = require('express-flash')
 
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 
 
-// mongoose.connect('mongodb://localhost/product', {
-//     useNewUrlParser: true
-// });
-// mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/orders', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise;
 
 app.use(express.static( __dirname + '/public/dist/public' ));
 app.use(express.static(__dirname + "/static"));
@@ -27,14 +26,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(session({
-    secret: "Over 9000!",
-    saveUninitialized: true,
-    resave: false,
-    cookie: {
-        maxAge: 60000
-    }
-}))
+
 
 
 
